28d9d83933044a1dcb6ccfbbae77fbec3b0f3510
Mrenty21 <erentestek2011@gmail.com>
2023-04-17 19:39:41 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sayi1 = 0;
+    int sayi2 = 1;
+
+    while(sayi1 < 50){
+        sayi1 = sayi2 + sayi1;
+        cout << sayi1 << endl;
+        sayi2 = sayi1 + sayi2;
+        cout << sayi2 << endl;
+    }
+
+    return 0;
+}
